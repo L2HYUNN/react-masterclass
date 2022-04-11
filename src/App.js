748c21406af6c95f4d7e1a0fec97ed4d3f6d6418
L2HYUNN <dhl9810@naver.com>
2022-04-11 23:28:02 +0900

    #2.2 Adapting and Extending

--- src/App.js
+++ src/App.js
@@ -4,16 +4,14 @@ const Father = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: teal;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
 
-const BoxTwo = styled.div`
-  background-color: red;
-  width: 100px;
-  height: 100px;
+const Circle = styled(Box)`
+  border-radius: 50px;
 `;
 
 const Text = styled.span`
@@ -24,10 +22,8 @@ const Text = styled.span`
 function App() {
   return (
     <Father>
-      <BoxOne>
-        <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo />
+      <Box bgColor="teal" />
+      <Circle bgColor="tomato" />
     </Father>
   );
 }
