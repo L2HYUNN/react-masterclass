c1f9df8d4072b010c0ac46710f0a7a827074e3f4
L2HYUNN <dhl9810@naver.com>
2022-05-02 01:00:15 +0900

    #2.4 Animations and Pseudo Selectors

--- src/App.js
+++ src/App.js
@@ -1,42 +1,48 @@
 import styled from "styled-components";
+import { keyframes } from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
 `;
 
+const rotateAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50% {
+    transform: rotate(360deg);
+    border-radius: 100px;
+  }
+  100% {
+    transform: rotate(360deg);
+    border-radius: 0px;
+  }
+`;
+
 const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
-
-const Circle = styled(Box)`
-  border-radius: 50px;
-`;
-
-const Title = styled.h1`
-  font-size: 30px;
-  color: aliceblue;
-`;
-
-const Btn = styled.button`
-  color: white;
+  height: 200px;
+  width: 200px;
   background-color: tomato;
-  border: 0;
-  border-radius: 15px;
-`;
-
-const Link = styled(Btn)``;
-
-const Input = styled.input.attrs({ required: true })`
-  background-color: tomato;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotateAnimation} 2s linear infinite;
+  span {
+    font-size: 36px;
+    &:hover {
+      font-size: 40px;
+    }
+  }
 `;
 
 function App() {
   return (
-    <Father as="header">
-      <Input />
-    </Father>
+    <Wrapper>
+      <Box>
+        <span>Hi</span>
+      </Box>
+    </Wrapper>
   );
 }
 
