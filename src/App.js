2b9f32c94788c861346c3edba6f07ef3c558d055
L2HYUNN <dhl9810@naver.com>
2022-05-04 23:44:35 +0900

    #2.7 Themes

--- src/App.js
+++ src/App.js
@@ -1,53 +1,22 @@
 import styled from "styled-components";
-import { keyframes } from "styled-components";
 
 const Wrapper = styled.div`
   display: flex;
-`;
-
-const rotateAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-  50% {
-    transform: rotate(360deg);
-    border-radius: 100px;
-  }
-  100% {
-    transform: rotate(360deg);
-    border-radius: 0px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  display: flex;
+  height: 100vh;
+  width: 100vw;
   justify-content: center;
   align-items: center;
-  animation: ${rotateAnimation} 2s linear infinite;
-  ${Emoji} {
-    &:hover {
-      font-size: 98px;
-    }
-    &:active {
-      opactiy: 0;
-    }
-  }
+  background-color: ${(props) => props.theme.backgroundColor};
+`;
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji>Hi</Emoji>
-      </Box>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
