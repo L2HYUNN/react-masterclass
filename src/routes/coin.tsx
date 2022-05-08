2f7c7babfa525f73fd88b9d9308f932c29402d38
L2HYUNN <dhl9810@naver.com>
2022-05-08 23:50:03 +0900

    new coinId added

--- src/routes/coin.tsx
+++ src/routes/coin.tsx
@@ -1,8 +1,8 @@
 import { useParams } from "react-router-dom";
 
 function Coin() {
-  const { coinId } = useParams();
-  return <h1>Coin: {coinId}</h1>;
+  const { coinIds } = useParams();
+  return <h1>Coin: {coinIds}</h1>;
 }
 
 export default Coin;
