b4ea9daaa12f52622945e027c2e88556c2324322
L2HYUNN <dhl9810@naver.com>
2022-05-10 23:32:45 +0900

    Experiment Box

--- src/routes/coin.tsx
+++ src/routes/coin.tsx
@@ -1,8 +1,25 @@
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+
+interface BoxProps {
+  width?: number;
+  height?: number;
+};
+
+const Box = styled.div<BoxProps>`
+  width: 200;
+  height: 200;
+`;
 
 function Coin() {
-  const { coinIds } = useParams();
-  return <h1>Coin: {coinIds}</h1>;
+  const { coinId } = useParams();
+  return (
+    <div>
+      <h1>Coin: {coinId}</h1>
+      <Box></Box>
+    </div>
+  );
 }
 
 export default Coin;
