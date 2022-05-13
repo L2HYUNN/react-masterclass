c1e6c038fe2590b71e6282146181dfeb624b0a4f
L2HYUNN <dhl9810@naver.com>
2022-05-13 20:31:58 +0900

    #4.9 React Query part One

--- src/theme.ts
+++ src/theme.ts
@@ -1,8 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
-export const theme:DefaultTheme = {
+export const theme: DefaultTheme = {
   backgroundColor: "#2f3640",
   textColor: "#f5f6fa",
   accentColor: "#9c88ff",
 };
-
